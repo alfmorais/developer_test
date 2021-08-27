013e5752a33c07274c3fb606d3fff855db19cd5f
Alfredo de Morais <alfredneto@hotmail.com>
2021-08-27 19:43:21 -0300

    6° Commit: Realizado a migração do banco de dados models.py do app survey, e verificado no MySQL a migração do banco de dados

--- take5/survey/admin.py
+++ take5/survey/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import (Survey,
+                     SurveyQuestion,
+                     SurveyQuestionAlternative,
+                     SurveyUserAnswer)
+
 
 # Register your models here.
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    """
+    That class provide for admin interface a good reference
+    from database Survey.
+    """
+    list_display = ('survey_id',
+                    'survey_name')
+
+
+@admin.register(SurveyQuestion)
+class SurveyQuestionAdmin(admin.ModelAdmin):
+    """
+    That class provide for admin interface a good reference
+    from database Survey Question.
+    """
+    list_display = ('survey',
+                    'question_id',
+                    'question')
+
+
+@admin.register(SurveyQuestionAlternative)
+class SurveyQuestionAlternativeAdmin(admin.ModelAdmin):
+    """
+    That class provide for admin interface a good reference
+    from database Survey Question Alternative.
+    """
+    list_display = ('survey_question',
+                    'first_alternative',
+                    'second_alternative',
+                    'third_alternative')
+
+
+@admin.register(SurveyUserAnswer)
+class SurveyUserAnswerAdmin(admin.ModelAdmin):
+    """
+    That class provide for admin interface a good reference
+    from database Survey User Answer
+    """
+    list_display = ('user_answer',)
